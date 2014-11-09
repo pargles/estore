86d2aa5a7e06bba089c587262dbf16945bae5d69
Paulo <paulovitor.93@hotmail.com>
2014-11-09 12:28:04 -0800

    login part

--- application/models/customer_model.php
+++ application/models/customer_model.php
@@ -1,17 +1,22 @@
 <?php
 class Customer_model extends CI_Model {
 
-	function getAll()
-	{  
+	function getAll(){  
 		$query = $this->db->get('customers');
 		return $query->result('Customer');
 	}  
+	
+	function get($id){
+		$query = $this->db->get_where('customers',array('id' => $id));
+		
+		return $query->row(0,'Customer');
+	}
 	
 	function delete($id) {
 		return $this->db->delete("customers",array('id' => $id ));
 	}
 	
-	function insert($product) {
+	function insert($customer) {
 		return $this->db->insert("customers", array('first' => $custormer->first,
 				                                  'last' => $custormer->last,
 											      'login' => $custormer->login,
